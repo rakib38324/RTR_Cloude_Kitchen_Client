35cf6cf060384fe01c2bef0ea6de41faed3c4fbb
rakib38324 <rakib3832@gmail.com>
2022-11-07 23:37:40 +0600

    Commit the carosole

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,46 +2,29 @@ import React from 'react';
 import img1 from '../Banner/bannerImages/pic1.jpg'
 import img2 from '../Banner/bannerImages/pic2.jpg'
 import img3 from '../Banner/bannerImages/pic3.jpg'
-import './Banner.css'
-import BannerItems from './BannerItems/BannerItems';
-
-const bannerData = [
-    {
-        image: img1,
-        prev: 3,
-        id: 1,
-        next: 2,
-    },
-    {
-        image: img2,
-        prev: 1,
-        id: 2,
-        next: 3,
-    },
-    {
-        image: img3,
-        prev: 2,
-        id: 3,
-        next: 1,
-    }
-]
 
 const Banner = () => {
     return (
         <div>
-        <div className="carousel w-full py-10">
-
-            {
-                bannerData.map(slide => <BannerItems
-                key={slide.id}
-                slide={slide}             
-                ></BannerItems>)
-                                   
-            }
-
+            <div className="carousel mx-5 rounded-lg mt-10">
+                <div id="item1" className="carousel-item w-full">
+                    <img src={img1} className="w-full" />
+                </div>
+                <div id="item2" className="carousel-item w-full">
+                    <img src={img2} className="w-full" />
+                </div>
+                <div id="item3" className="carousel-item w-full">
+                    <img src={img3} className="w-full" />
+                </div>
+                
+            </div>
+            <div className="flex justify-center w-full py-2 mb-16 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a>
+                <a href="#item2" className="btn btn-xs">2</a>
+                <a href="#item3" className="btn btn-xs">3</a>
+            </div>
         </div>
-    </div>
-        
+
     );
 };
 
