257feba7ba80cdc014b8dee7ddfe2f27bd720aad
rakib38324 <rakib3832@gmail.com>
2022-11-08 17:48:10 +0600

    Commit the preview image

--- src/Pages/Home/Services/ServicesDetails/ServicesDetails.js
+++ src/Pages/Home/Services/ServicesDetails/ServicesDetails.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaArrowCircleRight } from 'react-icons/fa';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const ServicesDetails = () => {
     const service = useLoaderData();
-    const { _id, title, price, img,description,ratting } = service;
+    const { _id, title, price, img, description, ratting } = service;
     return (
         <div className="card card-compact m-5 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="" /></figure>
+
+            
+            <PhotoProvider>
+                <PhotoView src={img}>
+                <figure><img src={img} alt="" /></figure>
+                </PhotoView>
+            </PhotoProvider>
+
+
             <div className="card-body">
                 <h2 className="card-title text-3xl">{title}</h2>
                 <div className="card-actions justify-end">
                     <p className='text-2xl text-red-600 font-semibold'>Price: ${price}</p>
                     <div>
-                    <p className='text-xl text-red-600 font-semibold'>Rating: {ratting}</p>
+                        <p className='text-xl text-red-600 font-semibold'>Rating: {ratting}</p>
                     </div>
                     <p>{description}</p>
                     <Link >
